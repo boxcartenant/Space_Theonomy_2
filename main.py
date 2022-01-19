3f710e65ec67c2e8064c47326fceabf1b47b937a
boxcartenant <zactheslade@gmail.com>
2022-01-19 13:43:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,22 @@
 
 #data structure for map nodes:
 class mapnode:
-    def __init__(self, linkedNodes=None):
+    def __init__(self, linkedNodes=None, properties=None):
         self.linkedNodes = linkedNodes #a list of pointers [node, node, node] to adjacent nodes
+        self.properties = properties #a python dictionary of other properties. Things like "can spawn here", "owner", and "local deck"
 #...
 
 #code for searching the map for things, like an unowned node for a player to spawn on or something. We can fill in the criteria later
 def searchMap(node, parents = []):
     parents.append(node)
+    result = None
+    ## put the search criteria here...
+    # if child meets some criteria: 
+    #   result = child
     for child in node.linkedNodes:
-        ##here put the search criteria...
-        # if child meets some criteria: 
-        #   return child
         if child not in parents:
-            return spanMap(child, parents)
+            result, parents = spanMap(child, parents)
+    return result, parents
 #...
 
 
